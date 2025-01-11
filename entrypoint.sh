0e8e81cbc031d49df28bce7fbc65a84060745c8f
iEdgir01 <fgteiw@gmail.com>
2025-01-11 02:15:57 +0200

    entrypoint correction

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,25 @@ check_env_vars() {
     STRIKE_COUNT=${STRIKE_COUNT:-5}
 }
 
+# Check for required config variables
+check_config_vars() {
+    [ -z "$RADARR_URL" ] && missing_vars+=("RADARR_URL")
+    [ -z "$RADARR_API_KEY" ] && missing_vars+=("RADARR_API_KEY")
+    [ -z "$SONARR_URL" ] && missing_vars+=("SONARR_URL")
+    [ -z "$SONARR_API_KEY" ] && missing_vars+=("SONARR_API_KEY")
+}
+
+# Ensure the /app/config directory exists
+setup_config_directory() {
+    if [ ! -d "/app/config" ]; then
+        log_info "/app/config directory not found. Creating it."
+        mkdir -p "/app/config"
+        chmod -R 777 "/app/config"
+    else
+        log_info "/app/config directory exists."
+    fi
+}
+
 # Update config.yml with environment variables
 update_config_yml() {
     envsubst < /app/config/config.template > /app/config/config.yml
@@ -37,8 +56,27 @@ setup_log_directory() {
 }
 
 # Main script execution
+setup_config_directory
 check_env_vars
+
+# Write or update config.yml with environment variables
 update_config_yml
+
+if [ -f /app/config/config.yml ]; then
+    log_info "Loading configuration from config.yml"
+    # Load the config file as environment variables
+    # Use sed to replace carriage returns and remove double quotes
+    while IFS= read -r line || [ -n "$line" ]; do
+        if [[ $line =~ ^[^#]*:[^#]*$ ]]; then
+            varname=$(echo "$line" | cut -d ':' -f 1 | tr -d '[:space:]')
+            varvalue=$(echo "$line" | cut -d ':' -f 2- | tr -d '[:space:]')
+            eval "$varname=\"$varvalue\""
+        fi
+    done < <(sed 's/\r//g' /app/config/config.yml)
+    check_config_vars
+else
+    log_info "config.yml not found."
+fi
 
 # Log and exit if any missing environment variables are found
 if [ ${#missing_vars[@]} -ne 0 ]; then
